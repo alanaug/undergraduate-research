71d2f72c6bd97df6ac4dec09ef6987455f43212b
Alan Oliveira <135788151+alanaug@users.noreply.github.com>
2023-08-12 21:05:33 -0300

    Add files via upload

---
+++ scripts/functions.js
@@ -0,0 +1,176 @@
+var engine = {
+    "cores": ['tesoura','purple'],
+    "hexadecimais":{
+      'tesoura':'url(https://m.media-amazon.com/images/I/814edBVQ1KL._UF1000,1000_QL80_.jpg)',
+      'purple':'url(https://media.geeksforgeeks.org/wp-content/uploads/image-after-1.png)',
+      'pink': '#F02A7E',
+      'red':'#E90808',
+      'yellow':'#E7D703',
+      'orange':'#F16529',
+      'grey':'#EBEBEB',
+      'black':'#141414',
+    },
+    "moedas":0
+}
+
+const audioMoeda = new Audio('audio/moeda.mp3');
+const audioErrou = new Audio('audio/errou.mp3');
+
+function sortearCor(){
+  var indexCorSorteada = Math.floor(Math.random() * engine.cores.length);
+  var legendaCorDaCaixa = document.getElementById('cor-na-caixa');
+  var nomeCorSorteada = engine.cores[indexCorSorteada];
+
+  legendaCorDaCaixa.innerText = nomeCorSorteada.toUpperCase();
+
+  return engine.hexadecimais[nomeCorSorteada];
+}
+
+
+function aplicarCorNaCaixa(nomeDaCor){
+  var caixaDasCores = document.getElementById('cor-atual');
+  
+  caixaDasCores.style.backgroundImage = nomeDaCor
+  caixaDasCores.style.backgroundSize = "100%";
+
+}
+
+
+function atualizaPontuacao(valor){
+  var pontuacao = document.getElementById('pontuacao-atual');
+
+  engine.moedas += valor;
+
+  if(valor < 0){
+    audioErrou.play();
+  }else{
+    audioMoeda.play();
+  }
+
+  pontuacao.innerText = engine.moedas;
+}
+
+aplicarCorNaCaixa(sortearCor())
+//API DE RECONHECIMENTO DE VOZ
+var btnGravador = document.getElementById("btn-responder");
+var transcricaoAudio = "";
+var respostaCorreta = "";
+
+if(window.SpeechRecognition || window.webkitSpeechRecognition){
+  var SpeechAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var gravador = new SpeechAPI();
+
+  gravador.continuos = false;
+  gravador.lang = "pt-BR";
+
+
+  gravador.onstart = function(){
+    btnGravador.innerText = "Estou Ouvindo";
+    btnGravador.style.backgroundColor = "white";
+    btnGravador.style.color = "black";
+  }
+
+  gravador.onend = function(){
+    btnGravador.innerText = "Responder";
+    btnGravador.style.backgroundColor = "transparent";
+    btnGravador.style.color = "white";
+  }
+
+  gravador.onresult = function(event){
+    transcricaoAudio = event.results[0][0].transcript.toUpperCase();
+    respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
+
+    if(transcricaoAudio ===  respostaCorreta){
+      atualizaPontuacao(1);
+    }else{
+      atualizaPontuacao(-1);
+    }
+
+    aplicarCorNaCaixa(sortearCor());
+
+  }
+
+
+}else{
+  alert('não tem suporte');
+}
+
+
+btnGravador.addEventListener('click', function(e){
+  gravador.start();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
